a85cb2dc36e6ce53d6b8cd35513c472a89352a28
Maja Petkova <majapetkova1@yahoo.com>
2024-11-08 13:09:56 +0100

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const PORT = `${import.meta.env.PORT}`
+export const PORT = 5555
 
-const dbname = `${import.meta.env.DB_NAME}`
-export const mongoDBUrl = `${import.meta.env.mongoDBUrl}/${dbname}`
+const dbname = "bookStore"
+export const mongoDBUrl = `mongodb://localhost:27017/${dbname}`
