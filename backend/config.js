141d2ca5d6b0365e9821c1b683be1267d8fd8a51
Maja Petkova <majapetkova1@yahoo.com>
2024-11-09 23:25:08 +0100

    Update mongoDB url

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555
 
-export const mongoDBUrl = `mongodb://localhost:27017/bookStore`
+export const mongoDBUrl = `mongodb+srv://majapetkova11:<DHeziu23VmAFpiR9>@cluster0.revi1.mongodb.net/`
